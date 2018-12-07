c294c6d3df6b3331421c59e267b279e366ffb291
jhreilly2007 <jhreilly2007@gmail.com>
2018-12-07 17:38:55 +0000

    some updates to recipe display (mainly time clock and source page info)

--- www/build/2.js
+++ www/build/2.js
@@ -1,10 +1,6 @@
 webpackJsonp([2],{
 
-<<<<<<< HEAD
 /***/ 405:
-=======
-/***/ 404:
->>>>>>> b8adae73467f3351f25602f0e5bf1054ba96e991
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -12,7 +8,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CardDisplayPageModule", function() { return CardDisplayPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(30);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__card_display__ = __webpack_require__(52);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__card_display__ = __webpack_require__(69);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
